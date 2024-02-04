2ecc5e7c1b2be041d11b2c433a0b67c2626e1d1a
hemishagawade <hemisha.d.g@gmail.com>
2024-02-04 19:08:04 +0530

    first commit

---
+++ madlib.py
@@ -0,0 +1,33 @@
+print("Let's play the Madlib Game !!")
+print("please enter the words of your choices")
+
+noun1 = input("Noun 1: ")
+noun2 = input("Noun 2: ")
+noun3 = input("Noun 3: ")
+plural_noun = input("Plural Noun: ")
+verb1 = input("Verb 1: ")
+verb2 = input("Verb 2: ")
+verb3 = input("Verb 3: ")
+verb4 = input("Verb 4: ")
+exclamation = input("Exclamation: ")
+adjective1 = input("Adjective 1: ")
+adjective2 = input("Adjective 2: ")
+adjective3 = input("Adjective 3: ")
+adjective4 = input("Adjective 4: ")
+animal = input("Animal: ")
+place = input("Place: ")
+emotion = input("Emotion: ")
+
+
+madlib = f"Once upon a time in the whimsical land of {adjective1}, there lived a peculiar \
+creature named Sir {noun1} the Brave. Sir {noun1} was known for his extraordinary talent in \
+{verb1} and his love for {plural_noun}. One day, he embarked on a quest to find the legendary \
+{adjective2} {noun2}, rumored to possess the power of {verb2}. Along the way, he encountered \
+a mischievous {animal}, who challenged him to a hilarious game of {verb3}. The two \
+competitors engaged in a(n) {adjective3} battle, using {exclamation} as their secret weapon. \
+The laughter echoed through the enchanted {place}, and even the trees couldn't help but \
+{verb4} along. In the end, Sir {noun1} emerged victorious, claiming the title of the \
+{adjective4} {noun3}. And so, the tale of Sir {noun1}'s absurd adventure became the talk \
+of the town, leaving everyone in stitches and {emotion} for days to come."
+
+print(madlib)
